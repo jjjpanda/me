88663fbda949b6f78e4347d39a46f3771007506e
jjjpanda <jawpanda13@gmail.com>
2020-05-21 17:22:08 -0400

    updates

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,6 +5,7 @@ import {
     Layout,
     Affix,
     Space,
+    Drawer,
     Typography
 } from 'antd'
 import {
@@ -14,15 +15,22 @@ import {
     Prompt
 } from 'react-router-dom';
 import {
+    UnorderedListOutlined,
     GithubOutlined,
     MailOutlined,
     LinkedinOutlined,
     InstagramOutlined,
-    FireOutlined
+    FireOutlined,
+    HomeOutlined, 
+    UserOutlined,  
+    ProfileOutlined,             
+    StarOutlined,
+    CommentOutlined,
 } from '@ant-design/icons'
 
 console.log(`url(${window.location+"img/gradient.png"})`)
 
+const Header = Layout.Header
 const Content = Layout.Content
 const Footer = Layout.Footer
 
@@ -30,63 +38,67 @@ class Main extends React.Component{
     constructor(props){
         super(props)
         this.state = {
-
+            visible: false
         }
+    }
+
+    closeDrawer = () => {
+        this.setState({
+            visible: false,
+        });
+    };
+
+    openDrawer = () => {
+        this.setState({
+            visible: true,
+        })
     }
 
     render() {
         return (
             <Router basename={'/me'} >
-                <Layout>
-                                      
-                    <Content>
 
-                        <Affix offsetTop={0}>
-                            <div style={{backgroundImage: `url(${window.location+"img/gradient.png"})`}}>
-                                <img style = {{height: "50px", width: "50px"}} src = "/me/img/blackIcon.png" />
-                            </div>
-                        </Affix>  
+                <Drawer
+                    title={<img onClick={this.closeDrawer} style = {{height: "50px", width: "50px"}} src = "/me/img/redIcon.png" />}
+                    width={"15vh"}
+                    placement="left"
+                    closable={false}
+                    drawerStyle={{ textAlign: "center" }}
+                    onClose={this.closeDrawer}
+                    visible={this.state.visible}
+                >
+                    <Space direction={"vertical"}>
+                        <HomeOutlined />
+                        <UserOutlined />
+                        <ProfileOutlined />
+                        <StarOutlined />
+                        <CommentOutlined />
+                    </Space>
+                </Drawer>
+
+                <Layout style={{ minHeight: "100vh" }}>
+
+                    <Header style={{ padding: '0px 5px', color: "white" }}>
+                        <Space onClick={this.openDrawer}>
+                            <UnorderedListOutlined />
+                            <Affix offsetTop={0}>
+                                <img  style = {{height: "50px", width: "50px"}} src = "/me/img/blackIcon.png" />
+                            </Affix>
+                        </Space>
+                    </Header>
+                                      
+                    <Content style={{ padding: '5px 20px' }}>
 
                         <Space direction="vertical">
 
                             <Typography>Hello world!</Typography>
                             <Typography>Oh, that's tacky.</Typography>
-                            
-                            <Button>Bruh</Button>
-                            <Button>Bruh</Button>
-                            <Button>Bruh</Button>
-                            <Button>Bruh</Button>
-                            <Button>Bruh</Button>
-                            <Button>Bruh</Button>
-                            <Button>Bruh</Button>
-                            <Button>Bruh</Button>
-                            <Button>Bruh</Button>
-                            <Button>Bruh</Button>
-                            <Button>Bruh</Button>
-                            <Button>Bruh</Button>
-                            <Button>Bruh</Button>
-                            <Button>Bruh</Button>
-                            
-                            <Button>Bruh</Button>
-                            <Button>Bruh</Button>
-                            <Button>Bruh</Button>
-                            <Button>Bruh</Button>
-                            <Button>Bruh</Button>                            
-                            <Button>Bruh</Button>
-                            <Button>Bruh</Button>
-                            <Button>Bruh</Button>
-                            <Button>Bruh</Button>
-                            <Button>Bruh</Button>
-                            <Button>Bruh</Button>
-                            <Button>Bruh</Button>
-                            <Button>Bruh</Button>
-                            <Button>Bruh</Button>
-
+                        
                         </Space>
                         
                     </Content>
 
-                    <Footer style={{textAlign: "center", color: "black"}}>
+                    <Footer style={{ padding: '0px 5px', textAlign: "center", color: "black" }}>
                         
                         Jay Pandya
 
@@ -99,7 +111,7 @@ class Main extends React.Component{
                             <LinkedinOutlined />
                             <FireOutlined />
                         </Space>
-
+                       
                     </Footer>
 
                 </Layout>
