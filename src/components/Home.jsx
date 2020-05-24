a1815a81ec5802c788c75814e328d4481db45902
jjjpanda <jawpanda13@gmail.com>
2020-05-23 22:40:17 -0400

    moving images

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,10 +11,6 @@ import {
 
 import '../css/animation.less'
 
-import IconMap from '../../docs/img/iconMap.svg'
-import MapBlack from '../../docs/img/mapBlack.svg'
-import MapWhite from '../../docs/img/mapWhite.svg'
-
 class Home extends React.Component{
     render(){
         return (
@@ -24,9 +20,6 @@ class Home extends React.Component{
                 <Typography>Oh, that's tacky.</Typography>
 
                 <div className="" >
-                    {/* <MapWhite id={"spinningFlicker1"} style={{ width: '30vh', height: 'auto'}}/>
-                    <IconMap id={"spinningFlicker2"} style={{ width: '30vh', height: 'auto'}}/>
-                    <MapBlack id={"spinningFlicker3"} style={{ width: '30vh', height: 'auto'}}/> */}
                     <img src={'/me/img/redIcon.png'} id={"spinningFlicker1"} style={{ width: '30vh', height: 'auto'}}/>
                     <img src={'/me/img/blackIcon.png'} id={"spinningFlicker2"} style={{ width: '30vh', height: 'auto'}}/>
                     <img src={'/me/img/blueIcon.png'} id={"spinningFlicker3"} style={{ width: '30vh', height: 'auto'}}/>
