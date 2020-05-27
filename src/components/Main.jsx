95bf4e652a5b1054ea6674d3464019b58b66bb41
jjjpanda <jawpanda13@gmail.com>
2020-05-26 23:14:31 -0400

    updates to menu

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -29,11 +29,11 @@ import Profile from './Profile.jsx'
 import Projects from './Projects.jsx'
 import Contact from './Contact.jsx'
 
-import SideDrawer from './SideDrawer.jsx'
 import NavMenu from './NavMenu.jsx';
 
 console.log(`url(${window.location+"img/gradient.png"})`)
 
+const Sider = Layout.Sider
 const Header = Layout.Header
 const Content = Layout.Content
 const Footer = Layout.Footer
@@ -42,112 +42,99 @@ class Main extends React.Component{
     constructor(props){
         super(props)
         this.state = {
-            visible: false,
-            hoverMenuVisible: false
+            menuOrientation: "vertical"
         }
     }
 
-    closeDrawer = () => {
-        this.setState({
-            visible: false,
-        });
-    };
-
-    openDrawer = () => {
-        this.setState({
-            visible: true,
-        })
+    toggleMenuOrientation = () => {
+        this.setState((oldState) => ({menuOrientation: oldState.menuOrientation == "horizontal" ? "vertical" : "horizontal"}))
     }
-
-    handleHoverMenuChange = visible => {
-        this.setState({
-            hoverMenuVisible: visible
-        });
-    };
 
     render() {
         return (
             <Router basename={'/me'} >
 
-                <SideDrawer openDrawer={this.openDrawer} closeDrawer={this.closeDrawer} visible={this.state.visible} />
-
                 <Layout style={{ minHeight: "100vh" }}>
 
-                    <Header style={{ padding: '0px 5px', color: "white" }}>
-                        <Popover 
-                            placement="bottom"
-                            content={
-                                <NavMenu onItemClick={() => {this.setState({hoverMenuVisible: false})}}/>
-                            }
-                            trigger="hover"
-                            onClick = {() => {
-                                this.handleHoverMenuChange(false)
-                            }}
-                            visible={this.state.hoverMenuVisible}
-                            onVisibleChange={this.handleHoverMenuChange}
-                            mouseEnterDelay = {0.680}
-                        >
-                            <Space onClick={this.openDrawer}>
-                                <UnorderedListOutlined />
-                                <Affix offsetTop={0}>
-                                    <img  style = {{height: "50px", width: "50px"}} src = "/me/img/blackIcon.png" />
-                                </Affix>
+                    {this.state.menuOrientation == "vertical" ? <Sider
+                        collapsed={true}
+                    >
+                        <Affix offsetTop={0}>
+                            <div style={{float: 'left', display: 'inline-block'}}>
+                                <img  style = {{height: "50px", width: "50px"}} src = "/me/img/blackIcon.png" onClick={this.toggleMenuOrientation} />
+                            </div>
+                        </Affix>
+                        <NavMenu mode="inline" />  
+                    </Sider> : null}
+
+                    <Layout >
+                    
+                        {this.state.menuOrientation == "horizontal" ? <Affix offsetTop={0}>
+                            <Header style={{ padding: '0px 5px', color: "white" }}>
+                                <div style={{float: 'left', display: 'inline-block'}}>
+                                    <img  style = {{height: "50px", width: "50px"}} src = "/me/img/blackIcon.png" onClick={this.toggleMenuOrientation} />
+                                </div>
+
+                                <div style={{float: 'right', display: 'inline-block'}}>
+                                    <NavMenu mode="horizontal" />  
+                                </div>
+                                                     
+                            </Header>
+                        </Affix> : null}
+
+                        <Content style={{ padding: '5px 20px' }}>
+                            
+                            <Route 
+                                path="/" 
+                                render={({location}) => {
+                                    if(location.search == "?about"){
+                                        return (
+                                            <About />
+                                        )
+                                    }
+                                    else if(location.search == "?profile"){
+                                        return (
+                                            <Profile />
+                                        )
+                                    }
+                                    else if(location.search == "?projects"){
+                                        return (
+                                            <Projects />
+                                        )
+                                        
+                                    }
+                                    else if(location.search == "?contact"){
+                                        return (
+                                            <Contact />
+                                        )
+                                    }
+                                    else {
+                                        return (
+                                            <Home />
+                                        )
+                                    }
+                                }} 
+                            />
+                            
+                        </Content>
+
+                        <Footer style={{ padding: '0px 5px', textAlign: "center", color: "black" }}>
+                            
+                            Jay Pandya
+
+                            <br />
+
+                            <Space>
+                                <GithubOutlined />
+                                <MailOutlined />
+                                <InstagramOutlined />
+                                <LinkedinOutlined />
+                                <Music />
                             </Space>
-                        </Popover>
-                    </Header>
-                                      
-                    <Content style={{ padding: '5px 20px' }}>
                         
-                        <Route 
-                            path="/" 
-                            render={({location}) => {
-                                if(location.search == "?about"){
-                                    return (
-                                        <About />
-                                    )
-                                }
-                                else if(location.search == "?profile"){
-                                    return (
-                                        <Profile />
-                                    )
-                                }
-                                else if(location.search == "?projects"){
-                                    return (
-                                        <Projects />
-                                    )
-                                    
-                                }
-                                else if(location.search == "?contact"){
-                                    return (
-                                        <Contact />
-                                    )
-                                }
-                                else {
-                                    return (
-                                        <Home />
-                                    )
-                                }
-                            }} 
-                        />
-                        
-                    </Content>
+                        </Footer>
 
-                    <Footer style={{ padding: '0px 5px', textAlign: "center", color: "black" }}>
-                        
-                        Jay Pandya
-
-                        <br />
-
-                        <Space>
-                            <GithubOutlined />
-                            <MailOutlined />
-                            <InstagramOutlined />
-                            <LinkedinOutlined />
-                            <Music />
-                        </Space>
-                       
-                    </Footer>
-
+                    </Layout>
                 </Layout>
             </Router>
         )
