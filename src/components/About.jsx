675ed38ae80f654ec8555ddeff8cf3ec8ef4c55f
jjjpanda <jawpanda13@gmail.com>
2020-05-27 17:38:36 -0400

    icon updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,10 +9,6 @@ import {
     Typography
 } from 'antd'
 
-import IconMap from '../../docs/img/iconMap.svg'
-import MapBlack from '../../docs/img/mapBlack.svg'
-import MapWhite from '../../docs/img/mapWhite.svg'
-
 class About extends React.Component{
     render(){
         return (
@@ -21,10 +17,6 @@ class About extends React.Component{
                 <Typography.Title>About me</Typography.Title>
                 <Typography>I'm Jay</Typography>
 
-                <MapWhite id={"spinningFlicker1"} style={{ width: '30vh', height: 'auto'}}/>
-                <IconMap id={"spinningFlicker2"} style={{ width: '30vh', height: 'auto'}}/>
-                <MapBlack id={"spinningFlicker3"} style={{ width: '30vh', height: 'auto'}}/>
-            
             </Space>
         )
     }
