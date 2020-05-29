85eb0ac04f3be8e15cc81086eb2ebc8c8324642c
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2020-05-29 01:19:00 -0400

    loading entry

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,9 +5,85 @@ import {
     Layout,
     Affix,
     Space,
-    Drawer,
+    Tree,
     Typography
 } from 'antd'
+import {
+    GithubOutlined,
+    MailOutlined,
+    LinkedinOutlined,
+    PhoneOutlined,
+    ArrowDownOutlined
+} from '@ant-design/icons'
+
+const resume = [
+    {
+        key: "info",
+        title: "Me",
+        children: [
+            {
+                key: "name",
+                title: "Name: Jay Pandya",
+                isLeaf: true
+            },
+            {
+                key: "contact",
+                title: "Contact Info",
+                children: [
+                    {
+                        key: "phone",
+                        icon: <PhoneOutlined />,
+                        title: "Phone: (---) --- ----",
+                        isLeaf: true
+                    },
+                    {
+                        key: "email",
+                        title: "Email",
+                        children: [
+                            {
+                                key:"gmail",
+                                icon: <MailOutlined />,
+                                title: "jtpandya3@gmail.com",
+                                isLeaf: true
+                            },
+                            {
+                                key:"stevens",
+                                icon: <MailOutlined />,
+                                title: "jpandya3@stevens.edu",
+                                isLeaf: true
+                            },
+                        ]
+                    }
+                ]
+            },
+            {
+                key: "links",
+                title: "Links",
+                children: []
+            }
+        ]
+    },
+    {
+        key: "education",
+        title: "Education",
+        children: []
+    },
+    {
+        key: "workExperience",
+        title: "Work Experience",
+        children: []
+    },
+    {
+        key: "otherExperience",
+        title: "Other Experience",
+        children: []
+    },
+    {
+        key: "skills",
+        title: "Technical Skills",
+        children: []
+    }
+]
 
 class Profile extends React.Component{
     render(){
@@ -16,6 +92,8 @@ class Profile extends React.Component{
 
                 <Typography.Title>Bruh moment</Typography.Title>
                 <Typography>I'm Jay</Typography>
+
+                <Tree treeData={resume} showIcon switcherIcon={<ArrowDownOutlined />} />
             
             </Space>
         )
