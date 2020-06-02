2bb047212e30b7ad5b18374f9f2142147417b7ec
jjjpanda <jawpanda13@gmail.com>
2020-06-01 21:41:50 -0400

    update swipe and note placement for mobile

--- src/components/MobileTopMenu.jsx
+++ src/components/MobileTopMenu.jsx
@@ -14,6 +14,11 @@ import {
 } from 'react-router-dom';
 
 import TopIcon from './TopIcon.jsx'
+import note from './note.jsx'
+
+import Cookie from 'js-cookie'
+
+let enter, exit = 0;
 
 class MobileTop extends React.Component{
     constructor(props){
@@ -26,35 +31,61 @@ class MobileTop extends React.Component{
                 }
             ],
             paths: ["/", "/?about", "/?profile", "/?projects", "/?contact"],
+            toggles: Cookie.get('swipeToggled') == 'toggled' ? NaN : 0,
         }
     }
 
     render() {
         const pathName = this.props.location.pathname+this.props.location.search
         return (
-            <SwipeAction 
-                autoClose
-                onOpen={() => {
-                    this.setState(() => {
-                        this.props.history.push(this.state.paths[(this.state.paths.findIndex(p => p === pathName) + 1) % this.state.paths.length])
-                        return {swipe: []}
-                    }, () => {
-                        this.setState({swipe: [
-                            {
-                                text: ' ',
-                                style: {backgroundColor: "purple"}
-                            }
-                        ]})
-                    })
-                    
+            <div 
+                onTouchStart={(e) => {
+                    enter = e.touches[0].screenX
+                    if(this.state.toggles == 10){
+                        note('info', "A Little Secret", 'Try swiping at the the top bar 😉', 5, "bottomRight")
+                    }
+                    else if(this.state.toggles < 10){
+                        this.setState((oldState) => ({toggles: oldState.toggles+1}))
+                    }
+                    //console.log(enter)
                 }}
-                right={this.state.swipe}
+                onTouchMove={(e) => {
+                    exit = e.touches[0].screenX
+                    //console.log(exit)
+                }}
             >
-                <NavBar 
-                    mode={"dark"}
-                    leftContent={<div style={{height: "inherit"}}> <TopIcon mobile /> </div>}
-                />
-            </SwipeAction>
+                <SwipeAction 
+                    autoClose
+                    onOpen={() => {
+                        this.setState(() => {
+                            //console.log(enter, exit, exit > enter)
+                            
+                            Cookie.set("swipeToggled", 'toggled', {expires: 10000})
+                            if(this.state.toggles < 10){
+                                note('success', "Easter Egg Hunter", 'Yes, swiping at the the top bar will allow you to cycle through the pages.\n Let\'s see if you can find more secrets 😅', 7, "bottomRight")
+                            }
+                           
+                            this.props.history.push(this.state.paths[(this.state.paths.findIndex(p => p === pathName) + (enter > exit ? 1 : this.state.paths.length-1)) % this.state.paths.length])
+                            return {swipe: [], toggles: (this.state.toggles < 10 ? NaN : this.state.toggles)}
+                        }, () => {
+                            this.setState({swipe: [
+                                {
+                                    text: ' ',
+                                    style: {backgroundColor: "purple"}
+                                }
+                            ]})
+                        })
+                        
+                    }}
+                    right={this.state.swipe}
+                    left={this.state.swipe}
+                >
+                    <NavBar 
+                        mode={"dark"}
+                        leftContent={<div style={{height: "inherit"}}> <TopIcon mobile /> </div>}
+                    />
+                </SwipeAction>
+            </div>
         )
     }
 }
