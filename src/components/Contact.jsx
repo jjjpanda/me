94f1d39a926936382d50e1cc25390173ff29a5b4
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2021-05-22 13:32:26 -0400

    update to contact fetch url

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -71,7 +71,7 @@ class Contact extends React.Component{
         }
 
         this.setState(() => ({submitted: "loading"}), () => {
-            fetch("http://localhost:8182/contact", {
+            fetch("https://jaeme.herokuapp.com/contact", {
                 method: "POST",
                 headers: {
                     "Accept": 'application/json',
