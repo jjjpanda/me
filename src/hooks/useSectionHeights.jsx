f9367525d1894bdc1104f4dd8f797d14be437bc5
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2024-05-29 20:55:38 -0400

    updates, closing #37 #38 #46

--- src/hooks/useSectionHeights.jsx
+++ src/hooks/useSectionHeights.jsx
@@ -1,8 +1,9 @@
 import {useCallback, useState, useEffect} from 'react'
 import { useWindowEvent, useWindowScroll  } from '@mantine/hooks';
 
-const useSectionHeights = (refArr) => {
+const useSectionHeights = (refArr, mobile=false) => {
     const [sectionHeights, setSectionHeights] = useState([])
+    const [activeSection, setActiveSection] = useState("");
 
     const updateSectionHeights = useCallback(() => {
         console.log("updating section heights")
@@ -32,26 +33,36 @@ const useSectionHeights = (refArr) => {
         if(sectionHeights.length > 0){
             const sectionToScrollTo = sectionHeights.find(section => section.key === sectionKey);
             console.log("scroll to", sectionKey, sectionToScrollTo)
-            scrollTo({
-                x: 0, 
-                y: sectionToScrollTo.height + 1
-            })
+            if(mobile){
+                window.scrollTo(
+                    0,
+                    sectionToScrollTo.height + 1
+                )
+            }else{
+                scrollTo({
+                    x: 0, 
+                    y: sectionToScrollTo.height + 1
+                })
+            }
         }
     }, [...refArr.map(({ ref }) => ref?.current), activeSection, sectionHeights])
 
-    const isActiveSectionPossible = sectionHeights.length > 0 && scroll.y > 0;
-    let activeSection;
-    if(isActiveSectionPossible){
-        activeSection = sectionHeights.reduce((possibleKey, currentSection) => {
-            const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
-            if(scroll.y >= Math.max(0, currentSection.height - windowHeight/3)){ //66% of scroll box is within section
-                return currentSection.key
-            }
-            else{
-                return possibleKey
-            }
-        }, null)
-    }
+    useEffect(() => {
+        const isActiveSectionPossible = sectionHeights.length > 0 && scroll.y > 0;
+        if(isActiveSectionPossible){
+            setActiveSection(() => {
+                return sectionHeights.reduce((possibleKey, currentSection) => {
+                    const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+                    if(scroll.y >= Math.max(0, currentSection.height - windowHeight/3)){ //66% of scroll box is within section
+                        return currentSection.key
+                    }
+                    else{
+                        return possibleKey
+                    }
+                }, null)
+            })
+        }
+    }, [scroll.x, scroll.y])
 
     console.log("section scrolling", scroll, sectionHeights)
 
